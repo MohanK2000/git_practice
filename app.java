51ec32b7495ccbb1410020c8b16dd1a90a3f8538
MohanK2000 <mohankandisa2000@gmail.com>
2024-02-11 17:05:57 +0530

    This is the stashed  data sent to app.java

--- app.java
+++ app.java
@@ -2,3 +2,4 @@ Hello World
 this is my first Git Class[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[T[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
 Hi
 This is my 1st GIT class
+this is a test to edit under hidden using stash
